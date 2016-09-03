15ffd127de77184d465342404486751b7971e127
ajayara3 <anandjyrm@gmail.com>
2016-09-03 02:58:56 -0400

    Hello world

---
+++ fend.c
@@ -0,0 +1,23 @@
+/* OS_Sandbox
+ * Anand Jayaram
+ * fend.c
+ */
+//#include <sys/ptrace.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+//#include <sys/user.h>
+//#include <asm/ptrace.h>
+//#include <sys/wait.h>
+//#include <asm/unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+//#include <sys/time.h>
+//#include <sys/types.h>
+#include "fend.h"
+int main() {
+	printf(Str);
+	return 0;
+}
---
+++ fend.h
@@ -0,0 +1,1 @@
+char Str[] = "Hello World";
